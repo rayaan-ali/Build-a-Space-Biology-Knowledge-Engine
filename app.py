6b781e36a285d8ea1dfd546d3958c9bf3ce52749
Yavuz Gebes <131059872+ytgebes@users.noreply.github.com>
2025-10-04 11:08:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
 import pandas as pd
-#import matplotlib.pyplot as plt
+'''
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import accuracy_score
@@ -21,6 +22,7 @@ from PIL import Image
 import io
 import json
 import requests
+'''
 import os
 
 
