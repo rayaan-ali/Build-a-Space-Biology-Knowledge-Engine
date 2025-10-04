ae767e393e11744f8b9d69255877be9c60610344
Yavuz Gebes <131059872+ytgebes@users.noreply.github.com>
2025-10-04 11:56:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import altair as alt
 from PIL import Image
 import io
 import json
-#from streamlit_extras.let_it_rain import rain
+from streamlit_extras.let_it_rain import rain
 
-#rain(emoji="✨", font_size=54, falling_speed=5, animation_length="infinite")
+rain(emoji="✨", font_size=54, falling_speed=5, animation_length="infinite")
 
 
 st.title("Simplified :blue[Knowledge]")
