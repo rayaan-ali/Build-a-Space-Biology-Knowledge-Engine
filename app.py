84bfbe91097312052515a553d5a9ad874525f7c7
Yavuz Gebes <131059872+ytgebes@users.noreply.github.com>
2025-10-04 10:52:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,80 @@
 import streamlit as st
 import google.generativeai as genai
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+import joblib
+import seaborn as sns
+import plotly.express as px
+from datetime import datetime, timedelta
+import calendar
+import sys
+import time
+import json
+import requests
 import os
 
+    st.markdown(
+    <style>
+        .block-container {
+            padding-top: 1rem; /* Adjust this value as needed */
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 st.title("Simplified :blue[Knowledge] :sunglasses:")
+
+st.write("Hello World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# The End
