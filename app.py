5f305039465c42e3a4c895ac360324e29147e8b8
rayaan-ali <a.rayaan009@gmail.com>
2025-10-04 11:19:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ from PIL import Image
 import io
 import json
 
-
-
-
-uploaded_file = st.file_uploader("Choose a file", type = "csv")
-
+uploaded_files = st.file_uploader("Upload CSV data", accept_multiple_files=True)
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        df = pd.read_csv(uploaded_file)
+        st.write(df)
+    
 st.title("Simplified :blue[Knowledge] :sunglasses:")
 
 st.write("Hello World")
