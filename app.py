9b7d4ba92243df7937d2aa5a2aac3d26d03b46bf
huh20 <147215793+huh20@users.noreply.github.com>
2025-10-04 10:44:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import os
 
     st.markdown(
-        <style>
+    <style>
         .block-container {
             padding-top: 1rem; /* Adjust this value as needed */
         }
