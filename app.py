035cc1ea0a56c93d402e73e8bd34e7babd8540ff
huh20 <147215793+huh20@users.noreply.github.com>
2025-10-04 10:48:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,4 @@ import streamlit as st
 import google.generativeai as genai
 import os
 
-    st.markdown(
-    <style>
-        .block-container {
-            padding-top: 1rem; /* Adjust this value as needed */
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
 st.title("Simplified :blue[Knowledge] :sunglasses:")
