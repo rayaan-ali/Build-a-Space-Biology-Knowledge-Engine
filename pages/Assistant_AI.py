f7c4b9e6e02e383b4469a4664d1b1c449865adf7
huh20 <147215793+huh20@users.noreply.github.com>
2025-10-05 10:41:37 -0400

    Update Assistant_AI.py

--- pages/Assistant_AI.py
+++ pages/Assistant_AI.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 #SETUP / Config
 st.set_page_config(page_title="Assistant AI", page_icon="💬", layout="wide")
 
-st.link_button("Go to Home Page","https://build-a-space-biology-knowledge-engine-2wcmupfwpshtv4uv2oxfap.streamlit.app/", *, help=None, type="secondary", icon=None, disabled=False, width="content")
+st.link_button("Go to Home Page","https://build-a-space-biology-knowledge-engine-2wcmupfwpshtv4uv2oxfap.streamlit.app/", help=None, type="secondary", icon="🏠", disabled=False, width="content")
 
 try:
     genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
